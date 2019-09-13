6e90729d171b0feb5ae1a33126ab0b2283305eb1
dpagidas <dim_pag@hotmail.com>
2019-09-13 16:15:23 +0300

    first commit

---
+++ movie/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Movie, MovieLinks
+
+
+admin.site.register(Movie)
+admin.site.register(MovieLinks)
---
+++ movie/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import MovieList , MovieDetail, MovieCategory, MovieLanguage , MovieSearch
+
+app_name = 'movie'
+
+urlpatterns = [
+    path('', MovieList.as_view(), name='movie_list'),
+    path('category/<str:category>', MovieCategory.as_view(), name='movie_category'),
+    path('language/<str:lang>', MovieLanguage.as_view(), name='movie_language'),
+    path('search/<str:query>', MovieSearch.as_view(), name='movie_search'),
+    path('<int:pk>', MovieDetail.as_view(), name='movie_detail'),
+]
+
